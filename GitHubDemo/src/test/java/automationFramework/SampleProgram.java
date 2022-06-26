bbab1c51a90713b45461ce7451ec445b0a0909be
Manjula <Manjula@LAPTOP-LD2PBR7V>
2022-06-26 07:29:13 +0530

    First Commit

---
+++ GitHubDemo/src/test/java/automationFramework/SampleProgram.java
@@ -0,0 +1,11 @@
+package automationFramework;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Today is Sunday");
+
+	}
+
+}
